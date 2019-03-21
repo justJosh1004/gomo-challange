a587f511d438cb625b007b6ecececcd8d90b1908
justJosh1004 <j.joson@live.com>
2019-03-21 10:15:05 -0400

    added initial function to check for gaps. commiting so I can push to GitHub

--- Challenge.js
+++ Challenge.js
@@ -1,4 +1,4 @@
-times = [
+const times = [
   {
     start: 19,
     end: 30
@@ -21,12 +21,37 @@ times = [
   }
 ];
 
+let sorted;
+let UVTChunks;
+
+// Take the time inputs and sort them in ascending order based on the start times
 const sortTimes = times => {
   return times.sort((a, b) => {
     return a.start - b.start;
   });
 };
 
-let sorted = sortTimes(times);
+// Once sorted, check to see if there are any gaps
+const checkGaps = sorted => {
+  // If there is a gap, splice out the ranges
+  sorted.map((time, i, sorted) => {
+    // console.log(sorted.length);
+    console.log(
+      `The time range is ${time.start}-${time.end}. this is number ${i +
+        1} on the list.`
+    );
+    // console.log(sorted[i + 1].end);
+    if (sorted[i + 1]) {
+      if (time.end > sorted[i + 1].start) {
+        console.log('There is no gap');
+      } else {
+        console.log('There is a gap');
+      }
+    }
+    console.log('');
+  });
+};
 
-console.log(sorted);
+sorted = sortTimes(times);
+
+checkGaps(sorted);
